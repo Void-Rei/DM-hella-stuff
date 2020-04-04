c944f8e6e86879f70e1ad2891b44fcf0caf2d128
alexserovvv <58313615+alexserovvv@users.noreply.github.com>
2020-04-04 03:48:36 +0300

    1 версия
    1 версия не утвержденная с остальными по соотношению функций

---
+++ DIV_NN_Dk.cpp
@@ -0,0 +1,29 @@
+//Назначение:
+//Функция для умножения натурального числа на 10 в степени k
+//Студент: Серов Алексей 9309
+//Возвращаемое значение: Отсутствует
+//Параметры:
+//Изменяемое число
+//Степень множителя 10
+
+void MUL_Nk_N(DM_N &obj, unsigned short k)
+{
+    DM_N temp;        //Временная переменная для хранения старых данных
+    temp.a = obj.a;
+    temp.n = obj.n;
+
+    short uselessZeroes = 0;
+    for (short i = temp.n - 1; i >= 0 && obj.a[i] == 0; i--)
+        ++uselessZeroes;
+
+    if (k > uselessZeroes)
+        obj.n += k - uselessZeroes;
+    obj.a = new unsigned short[obj.n];
+
+    for (short i = obj.n - 1; i >= 0; i--)
+        if (i < k)
+            obj.a[i] = 0;
+        else
+            obj.a[i] = temp.a[i - k];
+}
+
